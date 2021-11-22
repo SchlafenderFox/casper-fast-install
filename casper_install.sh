62346d61fda209649af6bdedf941b95d49c50639
SchlafenderFox <38919092+SchlafenderFox@users.noreply.github.com>
2021-11-22 19:11:42 +0200

    Update casper_install.sh

--- casper_install.sh
+++ casper_install.sh
@@ -93,6 +93,9 @@ sudo -u casper /etc/casper/config_from_example.sh 1_3_4
 sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_1
 sudo -u casper /etc/casper/config_from_example.sh 1_4_1
 
+sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_2
+sudo -u casper /etc/casper/config_from_example.sh 1_4_2
+
 sudo logrotate -f /etc/logrotate.d/casper-node
 sudo systemctl start casper-node-launcher; sleep 2
 systemctl status casper-node-launcher
